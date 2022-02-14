2eac6775ae2d7e54c664d327140a121c7a3c5290
Leonardo Santini <leonardo.santini@outlook.com.br>
2022-02-14 16:28:54 -0300

    Adicionado Card, Form, Navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+// Import funções react
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Import de componentes
+import { Navbar } from "./Components/Navbar";
+
 function App() {
-  return <div className="App">Oi</div>;
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="product/:_id" element={<Product />} />
+        <Route path="/addProduct" element={<Form />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
