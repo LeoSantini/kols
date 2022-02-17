232d74d905d5c5d963b094bb950b2e3ad77ef404
LeoSantini <95887191+LeoSantini@users.noreply.github.com>
2022-02-17 02:33:12 -0300

    Custom style

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,21 +1,20 @@
 import React from "react";
 import Resupply from "../../Components/Resupply/Resupply";
-import Profit from "../../Components/Profit/Profit"
-import TotalStock from "../../Components/TotalStock/TotalStock"
+import Profit from "../../Components/Profit/Profit";
+import TotalStock from "../../Components/TotalStock/TotalStock";
 
 function Home({ stock, isLoading }) {
-
-
-    return (
-        <>{!isLoading &&
-            <>
-                <Resupply stock={stock} isLoading={isLoading} />
-                <Profit stock={stock} isLoading={isLoading} />
-                <TotalStock stock={stock} isLoading={isLoading} />
-            </>
-        }
+  return (
+    <>
+      {!isLoading && (
+        <>
+          <TotalStock stock={stock} isLoading={isLoading} />
+          <Profit stock={stock} isLoading={isLoading} />
+          <Resupply stock={stock} isLoading={isLoading} />
         </>
-    );
+      )}
+    </>
+  );
 }
 
 export default Home;
